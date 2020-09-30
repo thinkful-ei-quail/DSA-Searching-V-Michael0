9e79ea8b5065a3436bc0c457d4b7d493a7102389
Michael Oldacre <mloldacre@live.com>
2020-09-30 15:00:47 -0400

    #1 done

--- src/search.js
+++ src/search.js
@@ -7,7 +7,9 @@ function indexOf(array, value) {
   return -1;
 };
 
-function binarySearch(array, value, start, end) {
+function binarySearch(array, value, start, end, counter = 1) {
+  console.log("counter:", counter);
+  let count = counter + 1
   var start = start === undefined ? 0 : start;
   var end = end === undefined ? array.length : end;
 
@@ -18,16 +20,19 @@ function binarySearch(array, value, start, end) {
   const index = Math.floor((start + end) / 2);
   const item = array[index];
 
-  console.log(start, end);
+  //console.log(start, end);
   if (item == value) {
     return index;
   }
   else if (item < value) {
-    return binarySearch(array, value, index + 1, end);
+    return binarySearch(array, value, index + 1, end, count);
   }
   else if (item > value) {
-    return binarySearch(array, value, start, index - 1);
+    return binarySearch(array, value, start, index - 1, count);
   }
 };
 
-binarySearch([3, 5, 6, 8, 11, 12, 14, 15, 17, 18]);
+const arr = [3, 5, 6, 8, 11, 12, 14, 15, 17, 18, 19, 20, 24, 33, 36, 40, 41, 45, 57, 68];
+
+//console.log(binarySearch(arr, 8)); 
+console.log(binarySearch(arr, 8)); 
