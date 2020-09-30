3aa1f8b8c71e096d2cad9898d28d7aff342e6c11
Michael Oldacre <mloldacre@live.com>
2020-09-30 14:37:34 -0400

    Setup

---
+++ src/search.js
@@ -0,0 +1,33 @@
+function indexOf(array, value) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == value) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+function binarySearch(array, value, start, end) {
+  var start = start === undefined ? 0 : start;
+  var end = end === undefined ? array.length : end;
+
+  if (start > end) {
+    return -1;
+  }
+
+  const index = Math.floor((start + end) / 2);
+  const item = array[index];
+
+  console.log(start, end);
+  if (item == value) {
+    return index;
+  }
+  else if (item < value) {
+    return binarySearch(array, value, index + 1, end);
+  }
+  else if (item > value) {
+    return binarySearch(array, value, start, index - 1);
+  }
+};
+
+binarySearch([3, 5, 6, 8, 11, 12, 14, 15, 17, 18]);
